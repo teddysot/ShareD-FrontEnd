cb58fca72625a4732a672212510c2ffedc8d04bc
Teddy <earth_sot@hotmail.com>
2020-11-20 17:34:19 +0700

    Add redux to project

--- src/index.js
+++ src/index.js
@@ -3,11 +3,37 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'antd/dist/antd.css';
+import { Provider } from 'react-redux'
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import { BrowserRouter } from 'react-router-dom';
+import socketReducer from './store/reducers/socket'
+import roleReducer from './store/reducers/role'
+import thunk from 'redux-thunk'
+
+const rootReducer = combineReducers({
+  socket: socketReducer,
+  role: roleReducer
+})
+
+const logger = (store) => {
+  return (next) => {
+    return (action) => {
+      console.log(`[Middleware] Dispatching`);
+      const result = next(action)
+      console.log(`[Middleware] next state`);
+      return result
+    }
+  }
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger, thunk)));
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />,
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
